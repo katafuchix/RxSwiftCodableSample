f4aa589e7a6cf7e7c1dde4f48b598117a6b708b0
katafuchix <katafuchi@deskplate.net>
2018-06-20 14:25:23 +0900

    Initial Commit

---
+++ RxSwiftCodableSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftCodableSample
+//
+//  Created by cano on 2018/06/20.
+//  Copyright © 2018年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
